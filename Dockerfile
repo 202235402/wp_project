fa98f8138e772c1459d5c41630657266e8f11382
minju kang <kmj20030421@gmail.com>
2024-05-09 03:19:04 +0900

    init

---
+++ Dockerfile
@@ -0,0 +1,9 @@
+FROM python:3.10
+
+WORKDIR /code
+
+COPY ./requirements.txt /code/requirements.txt
+
+RUN pip3 install --no-cache-dir --upgrade -r /code/requirements.txt 
+
+#Docker이미지를 빌드하는데 필요한 지시문을 포함. python환경을 정의하고 필요한 의존성을 설치한 후 애플리케이션을 실행하는데 필요한 명령을 정의
---
+++ app/main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from pydantic import BaseModel
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///../todo.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+app = FastAPI()
+
+class Todo(Base):
+    __tablename__ = "todos"
+    __allow_unmapped__ = True
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String, index=True)
+
+Base.metadata.create_all(bind=engine)
+
+class TodoCreate(BaseModel):
+    title: str
+    description: str
+class TodoUpdate(BaseModel):
+    title: str
+    description: str
+
+def get_db():
+    db = SessionLocal()
+    try: 
+        yield db
+    finally:
+        db.close()
+
+@app.post("/todos/", response_model=TodoCreate)
+def create_todo(todo: TodoCreate, db: Session = Depends(get_db)):
+    db_todo = Todo(title=todo.title, description=todo.description)
+    db.add(db_todo)
+    db.commit()
+    db.refresh(db_todo)
+    return db_todo
+
+@app.get("/todos/{todo_id}")
+def read_todo(todo_id: int, db: Session = Depends(get_db)):
+    db_todo = db.query(Todo).filter(Todo.id == todo_id).first()
+    if db_todo is None:
+        raise HTTPException(status_code=404, detail="Todo not found")
+    return db_todo
